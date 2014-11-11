0b3cb50e7af875abce496ad2dbebbbddcace6cfe
martin.paces <martin.paces@eox.at>
2014-11-11 03:03:44 +0100

    Version update.

--- eoxserver/__init__.py
+++ eoxserver/__init__.py
@@ -27,7 +27,7 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-VERSION = (0, 4, None, 'dev', 8)
+VERSION = (0, 4, None, 'dev', 9)
 #VERSION = (0, 4, None, 'alpha', 1)
 #VERSION = (0, 4, None, 'beta', 1)
 #VERSION = (0, 4, None, 'rc', 1)
